a2c959764d9531acebaf80cd17406cec5a0c96f4
freddiecabrera <freddiecabrera18@yahoo.com>
2016-03-16 01:18:30 -0700

    make back end work

---
+++ db/db.js
@@ -0,0 +1,10 @@
+var mysql = require("mysql")
+var connection = mysql.createConnection ({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "cards"
+})
+
+
+module.exports = connection;
---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var db = require("../db/db");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+
+router.get('/cards', function(req, res, next) {
+  db.query("SELECT * FROM flashcards", function(err, rows) {
+    if (err) {
+      return res.status(400).send(err)
+    }
+    res.send(rows);
+  });
+});
+
+router.post('/cards', function(req, res) {
+  db.query('INSERT INTO flashcards SET?', req.body, function(err, result) {
+    if (err) return res.status(400).send(err);
+    res.send(result);
+  });
+});
+
+router.delete("/cards", (req, res) => {
+    id = req.body.id;
+    db.query("DELETE FROM flashcards WHERE id=" + id, function(err, result) {
+          if (err) return res.status(400).send(err);
+          console.log('deleted' + result.affectedRows + 'rows');
+    });
+});
+
+router.put("/cards", (req, res) => {
+  id = req.body.id;
+  updateObj = req.body;
+  var updateDB = "UPDATE flashcards SET category='" + updateObj.category + "', question='" + updateObj.question + "', answer='" + updateObj.answer + "' where id='" + id + "'";
+  db.query(updateDB, function(err, result) {
+    console.log(updateDB);
+    res.send(result);
+  })
+})
+module.exports = router;
